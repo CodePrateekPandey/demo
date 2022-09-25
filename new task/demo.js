0499015cadf8de433143a90e6a49e3e751dbd7c7
CodePrateekPandey <prateekpandey212@gmail.com>
2022-09-25 19:20:47 +0530

    first commit

---
+++ new task/demo.js
@@ -0,0 +1,34 @@
+// console.log(document.URL);
+// console.log(document.body);
+// console.log(document.forms);
+// console.log(document.domain);
+// console.log(document.title);
+// document.title=123
+
+// console.log(document.head);
+// console.log(document.doctype);
+// console.log(document.children);
+// console.log(document.images);
+// console.log(document.all);
+// console.log(document.all[15]);
+// console.log(document.links);
+
+// geteliment by id
+
+// console.log(document.getElementById('header-title'));
+//  var headerTitle=document.getElementById('header-title');
+//  var header=document.getElementById('main-header');
+
+// //  console.log(headerTitle);
+// //  headerTitle.textContent='hello';
+// header.style.borderBottom='solid 3px #000';
+
+//  get eliment by  class name 
+
+var itam=document.getElementsByClassName('list-group-item');
+// console.log(itam);
+// console.log(itam[1]);
+ itam[1].textContent='hello';
+itam[1].style.fontWeight='bold';
+//itam[2].style.backgroundColor='red';
+itam[2].style.color='blue';
