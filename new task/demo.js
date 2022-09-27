ddc5f6b8495ac00e1c27407a03d814dd4424e562
CodePrateekPandey <prateekpandey212@gmail.com>
2022-09-27 13:14:01 +0530

    first commit

--- new task/demo.js
+++ new task/demo.js
@@ -25,10 +25,21 @@
 
 //  get eliment by  class name 
 
-var itam=document.getElementsByClassName('list-group-item');
+//var itam=document.getElementsByClassName('list-group-item');
 // console.log(itam);
 // console.log(itam[1]);
- itam[1].textContent='hello';
-itam[1].style.fontWeight='bold';
+//  itam[1].textContent='hello';
+// itam[1].style.fontWeight='bold';
 //itam[2].style.backgroundColor='red';
-itam[2].style.color='blue';
+// itam[2].style.color='blue';
+//itam[3].style.backgroundColor='#00FF00';
+
+
+// for(var i=0;i<=itam.length;i++){
+//     itam[i].style.backgroundColor='#00FF00';
+// }
+// querySalector
+ var header=document.querySelector('#main-header');
+ header.style.borderBottom='solid 5px #ccc';
+ 
+
